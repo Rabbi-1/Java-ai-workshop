c4502cf9df04fe8b49d53ea0f0402ca778a30cf9
Rabbi-1 <fuadhrabbi66@gmail.com>
2025-08-03 19:18:35 -0400

    First commit

---
+++ src/main/java/com/dev/Rabbi/java_ai_workshop/JavaAiWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.dev.Rabbi.java_ai_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAiWorkshopApplication.class, args);
+	}
+
+}
