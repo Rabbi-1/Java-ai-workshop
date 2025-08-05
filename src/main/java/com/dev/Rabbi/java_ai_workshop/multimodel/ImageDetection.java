08a482fe1dbe39b384714ca3c771b9a817d43c05
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-04 21:38:21 -0400

    Add ImageDetection that can describe an image.

--- src/main/java/com/dev/Rabbi/java_ai_workshop/multimodel/ImageDetection.java
+++ src/main/java/com/dev/Rabbi/java_ai_workshop/multimodel/ImageDetection.java
@@ -7,7 +7,6 @@ import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 
 @RestController
 public class ImageDetection {
